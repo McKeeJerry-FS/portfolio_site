243dcdb07fd7106c6c19f5a30a5ebcd01c5e43ad
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2023-11-22 14:04:14 -0500

    updated the blog section

---
+++ js/blogAPI.js
@@ -0,0 +1,77 @@
+function fetchBlogData() {
+    const baseURL = "https://techtalkblog-production.up.railway.app";
+    
+      fetch(`${baseURL}/api/BlogPosts/portfolio/3`)
+        .then((response) => response.json())
+        .then(function (data) {
+          displayBlogData(data, baseURL);
+        });
+  }
+
+function displayBlogData(blogPosts, baseUrl) {
+    let template = document.getElementById('blog-template');
+    let blogSection = document.getElementById('blog');
+
+    blogPosts.forEach((blogPost) => {
+        const blogPostCard = document.importNode(template.content, true);
+        //format image
+        let imageDiv = blogPostCard.querySelector('[data-blog="imageLink"]');
+        imageDiv.setAttribute(
+            "href",
+            `${baseUrl}/content/${blogPost.slug}`
+        );
+        imageDiv.href = `${baseUrl}/content/${blogPost.slug}`;
+
+        let imgTag = document.createElement('img');
+        imgTag.setAttribute(
+            "src",
+            `data:${blogPost.imageType};base64,${blogPost.imageData}`
+        );
+        imgTag.classList.add('blog-image');
+        imageDiv.appendChild(imgTag);
+        // <img src="data:image/gif;base64,xxxxxxxxxxxxx..." class="blog-image" alt="...">
+        //add title
+        let blogTitleDiv = blogPostCard.querySelector('[data-blog="title"]');
+        blogTitleDiv.innerHTML = blogPost.title;
+
+        let blogDate = new Date(blogPost.dateCreated); // 2009-11-10
+        let month = blogDate.toLocaleString('default', { month: 'long' });
+        let day = blogDate.getDate();
+
+        //add day
+        let blogDayDiv = blogPostCard.querySelector('[data-blog="day"]');
+        blogDayDiv.innerHTML = day;
+
+        //add month
+        let blogMonthDiv = blogPostCard.querySelector('[data-blog="month"]');
+
+        blogMonthDiv.innerHTML = month;
+
+        //add content
+        let blogContentDiv = blogPostCard.querySelector('[data-blog="content"]');
+        blogContentDiv.innerHTML = blogPost.content;
+
+        //readmore link
+        let blogLink = blogPostCard.querySelector('[data-blog="readMoreLink"]');
+        blogLink.setAttribute(
+            "href",
+            `${baseUrl}/content/${blogPost.slug}`
+        );
+
+        let blogPubDate = blogPostCard.querySelector('[data-blog="publishedDate"]');
+
+        let dateToday = new Date();
+        let createdDate = new Date(
+            blogPost.Updated != null ? blogPost.Updated : blogPost.Created
+        );
+        let diffTime = Math.abs(dateToday.getTime() - createdDate.getTime());
+        let lastUpdated = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        if (lastUpdated == 1) {
+            blogPubDate.innerHTML = `Published ${lastUpdated} day ago`;
+        } else {
+            blogPubDate.innerHTML = `Published ${lastUpdated} days ago`;
+        }
+
+        blogSection.appendChild(blogPostCard);
+    });
+}
---
+++ js/blogAPITest.js
@@ -0,0 +1,88 @@
+function fetchBlogData() {
+    const baseURL = "https://cf-blog-net6.herokuapp.com/";
+    
+      fetch(`https://techtalkblog-production.up.railway.app/api/BlogPosts/portfolio/3`)
+        .then((response) => response.json())
+        .then(function (data) {
+          displayBlog(data, baseURL);
+        });
+  }
+  
+  function displayBlog(data, baseURL) {
+    //blogs = getData();
+  
+    let template = document.getElementById("blog-template");
+    let blogSection = document.getElementById("blogs");
+    /* 
+    "title": "string",
+    "slug": "string",
+    "abstract": "string",
+    "content": "string",
+    "created": "2022-08-24",
+    "updated": "2022-08-24",  
+    "imageData": "string",
+    "imageType": "string",
+    */
+  
+    data.forEach((article) => {
+      const articleCard = document.importNode(template.content, true);
+      //format image
+      imageDiv = articleCard.querySelector('[data-blog="imageLink"]');
+      imageDiv.setAttribute(
+        "href",
+        `http://cf-blog-net6.herokuapp.com/content/${article.slug}`
+      );
+  
+      imgTag = document.createElement("img");
+      imgTag.setAttribute(
+        "src",
+        `data:${article.imageType};base64,${article.imageData}`
+      );
+      imgTag.classList.add("blog-image");
+      imageDiv.appendChild(imgTag);
+      // <img src="data:image/gif;base64,xxxxxxxxxxxxx..." class="blog-image" alt="...">
+      //add title
+      cardTitleDiv = articleCard.querySelector("[data-blog='title']");
+      cardTitleDiv.innerHTML = article.title;
+  
+      let blogDate = new Date(article.created); // 2009-11-10
+      let month = blogDate.toLocaleString("default", { month: "long" });
+      let day = blogDate.getDate();
+  
+      //add day
+      blogDayDiv = articleCard.querySelector("[data-blog='day']");
+      blogDayDiv.innerHTML = day;
+  
+      //add month
+      blogMonthDiv = articleCard.querySelector("[data-blog='month']");
+  
+      blogMonthDiv.innerHTML = month;
+  
+      //add content
+      blogContentDiv = articleCard.querySelector("[data-blog='content']");
+      blogContentDiv.innerHTML = article.content;
+  
+      //readmore link
+      blogLink = articleCard.querySelector("[data-blog='readMoreLink']");
+      blogLink.setAttribute(
+        "href",
+        `http://cf-blog-net6.herokuapp.com/content/${article.slug}`
+      );
+  
+      blogPubDate = articleCard.querySelector("[data-blog='publishedDate']");
+  
+      dateToday = new Date();
+      createdDate = new Date(
+        article.updated != null ? article.updated : article.created
+      );
+      diffTime = Math.abs(dateToday.getTime() - createdDate.getTime());
+      lastUpdated = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      if (lastUpdated == 1) {
+          blogPubDate.innerHTML = `Published ${lastUpdated} day ago`;
+      } else {
+          blogPubDate.innerHTML = `Published ${lastUpdated} days ago`;
+      }
+  
+      blogSection.appendChild(articleCard);
+    });
+  }
